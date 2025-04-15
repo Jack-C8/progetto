a45667551f5d1dc2a55aa7b313b45d6c4183ed0b
Jonathan Figoni <jonathan.figoni@studio.unibo.it>
2025-04-15 17:49:38 +0200

    Versione 2

--- card.cpp
+++ card.cpp
@@ -9,25 +9,20 @@
 // definition of card through a struct, easier and more efficient than a class
 
 struct Card {
-  std::string suit;
-  std::string range;
-  int game_value;
-  // nuovo operatore per confronto di carte!
-  Card(const std::string& suit, const std::string& range, int game_value) {
-    this->suit = suit;
-    this->range = range;
-    this->game_value = game_value;
-}
+  std::string suit_;
+  std::string range_;
+  int game_value_;
+  Card(const std::string& suit, const std::string& range, int game_value) : suit_{suit}, range_{range}, game_value_{game_value} {}
 
   bool operator==(const Card& other) const {
-    return suit == other.suit && range == other.range;
+    return suit_ == other.suit_ && range_ == other.range_;
   }
 };
 class Deck {
   std::vector<Card> cards;
 
  public:
-  // deck constructor with for-loop for create starting deck
+  // deck constructor with for-loop to create starting deck
   Deck() {
     std::string suits[] = {"Hearts", "Clubs", "Spades", "Diamonds"};
     std::string ranges[] = {"2", "3",  "4", "5", "6", "7", "8",
@@ -36,7 +31,7 @@ class Deck {
     // std::string suits[] is not a lambda but contains the possible value in
     // order.
 
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < 3; ++i) {
       for (int j = 0; j < 13; ++j) {
         cards.emplace_back(suits[i], ranges[j], game_value[j]);
       }
@@ -58,31 +53,7 @@ verificare che il mazzo abbia tutte le carte in modo corretto,
 verificare che il mescolamento funzioni e che il mazzo cambi ordine,
 verificare che i valori siano quelli giusti*/
 
-TEST_CASE("Deck shuffle") {
-  Deck deck1;
-  Deck deck2;
-
-  SUBCASE("Shuffled Deck") {
-    deck1.shuffle();
-    deck2.shuffle();
-
-    // mescolo i mazzi e con sto ciclo che almeno in una posizione le carte sono
-    // diverse il che vuol dire che i mazzi sono stati mischiati.
-    // iteratore arriva fino a 52 perché se trova un mazzo in ordine è già un
-    // problema.
-
-    bool different = false;
-    for (size_t i = 0; i < 52; ++i) {
-      if (!(deck1.getCards()[i] == deck2.getCards()[i])) {
-        different = true;
-        break;
-      }
-    }
-
-    CHECK(different == true);
-  }
-  SUBCASE("Number of cards after shuffle") {
-    deck1.shuffle();
-    CHECK(deck1.getNumberOfCards() == 52);
-  }
+int main() {
+  Card c{"spade", "3", 3};
+  std::cout << c.suit_;
 }
