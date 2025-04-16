df65aea822b38b45d2aef39b60c4831c5d310f02
Jack-C8 <giacomo.crescenzio05@gmail.com>
2025-04-16 12:19:08 +0200

    Add files via upload
    Final Version

--- card.cpp
+++ card.cpp
@@ -1,70 +1,53 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <algorithm>
 #include <random>
 #include <string>
 #include <vector>
-#include <iostream> //togli appena finito, serve per testare su due piedi con cout
 
-struct Card
-{
-  std::string suit_;
-  std::string range_;
-  int game_value_;
-  Card(const std::string &suit, const std::string &range, int game_value) : suit_{suit}, range_{range}, game_value_{game_value} {}
+struct Card {
+  std::string suit;
+  std::string range;
+  int game_value;
+  bool face;
 
-  bool operator==(const Card &other) const
-  {
-    return suit_ == other.suit_ && range_ == other.range_;
+  Card(const std::string& suit, const std::string& range, int game_value,
+       bool face = true)
+      : suit{suit}, range{range}, game_value{game_value}, face{face} {}
+
+  bool operator==(const Card& other) const {
+    if (suit == other.suit && range == other.range) {
+      return true;
+    }
+    return false;
   }
 };
 
-class Deck
-{
+class Deck {
   std::vector<Card> cards;
 
-public:
-  Deck()
-  {
+ public:
+  Deck() {
     std::string suits[] = {"Hearts", "Clubs", "Spades", "Diamonds"};
-    std::string ranges[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    std::string ranges[] = {"2", "3",  "4", "5", "6", "7", "8",
+                            "9", "10", "J", "Q", "K", "A"};
     int game_value[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
-    // std::string suits[] is not a lambda but contains the possible value in
-    // order.
 
-    for (int i{0}; i < 3; ++i)
-    {
-      for (int j{0}; j < 13; ++j)
-      {
-        cards.emplace_back(suits[i], ranges[j], game_value[j]);
+    for (int n = 0; n < 3; ++n) {
+      for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 13; ++j) {
+          cards.emplace_back(suits[i], ranges[j], game_value[j]);
+        }
       }
     }
   }
-
-  void shuffle()
-  {
+  void shuffle() {
     std::random_device rd;
     std::default_random_engine rng(rd());
     std::shuffle(cards.begin(), cards.end(), rng);
   }
 
-  Card draw() {                  // fatto io, va bene? -J
-    Card c = *cards.begin();
-    cards.erase(cards.begin());
-    return c;
+  Card drawCard() {
+    Card top = cards.back();
+    cards.pop_back();
+    return top;
   }
-  int size() const { return cards.size(); }  
-  //const std::vector<Card> &getCards() const { return cards; }     secondo me non serve -J
 };
-
-/*Test da implementare: 
-verifica della size di cards,
-verificare che il mazzo abbia tutte le carte in modo corretto,
-verificare che il mescolamento funzioni e che il mazzo cambi ordine,
-verificare che i valori siano quelli giusti, 
-verificare il funzionamento di erase (rimpicciolisce il mazzo di 1?)*/
-
-int main()
-{
-  Card c{"spade", "3", 3};
-  std::cout << c.suit_;
-}
