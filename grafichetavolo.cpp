41cf4fec9075c83acb92c82d932b0ce616d80afa
Salvatore <sasagritto05@gmail.com>
2025-05-07 11:27:58 +0200

    grafiche tavolo complete

--- grafichetavolo.cpp
+++ grafichetavolo.cpp
@@ -22,6 +22,18 @@ void drawcircle(sf::RenderWindow& window, float x, float y, float raggio,
   cerchio.setOutlineColor(bordi);
   window.draw(cerchio);
 }
+sf::RectangleShape RectangularButton(sf::RenderWindow& window, float x, float y,
+                                     float w, float h, sf::Color sfondo,
+                                     float thickness, sf::Color bordi,
+                                     float angle_of_rotation) {
+  sf::RectangleShape rect(sf::Vector2f(w, h));
+  rect.setPosition(x, y);
+  rect.setOutlineColor(bordi);
+  rect.setOutlineThickness(thickness);
+  rect.setFillColor(sfondo);
+  rect.setRotation(angle_of_rotation);
+  return rect;
+}
 
 void drawRect(sf::RenderWindow& window, float x, float y, float w, float h,
               sf::Color sfondo, float thickness, sf::Color bordi,
@@ -95,6 +107,25 @@ int main() {
 
   std::vector<sf::Text> allLetters;
 
+  sf::Texture texture;
+  if (!texture.loadFromFile("Fishcontainer.png")) {
+    return -1;  // errore se non trova l'immagine
+  }
+  sf::Texture texture2;
+  if (!texture2.loadFromFile("cardcontainer.png")) {
+    return -1;
+  }
+
+  sf::Sprite sprite;
+  sprite.setTexture(texture);
+  sprite.setScale(0.8f, 0.6f);
+  sprite.setPosition(638, 10);
+
+  sf::Sprite sprite2;
+  sprite2.setTexture(texture2);
+  sprite2.setScale(0.08f, 0.1f);
+  sprite2.setPosition(830, 10);
+
   auto curved1 = createCurvedText("BLACKJACK PAYS 3 TO 2", font, 15,
                                   {715.f, 15.f}, 260.f, 180.f, 180.f);
   auto curved2 =
@@ -107,11 +138,21 @@ int main() {
       createCircularPositions({715.f, 50.f}, 400.f, 180.f, 180.f, 10);
 
   int your_score{0};
-  sf::RectangleShape button(sf::Vector2f(60, 60));
-  button.setFillColor(sf::Color::Green);
-  button.setOutlineColor(sf::Color::White);
-  button.setPosition(550, 775);
-  button.setOutlineThickness(2);
+
+  float fishes_left{0.};
+  sf::RectangleShape double_button{
+      RectangularButton(window, 850, 810, 100, 50, sf::Color(150, 150, 150), 2,
+                        sf::Color::White, 0)};  // double button
+  sf::RectangleShape split_button{
+      RectangularButton(window, 980, 810, 100, 50, sf::Color(150, 150, 150), 2,
+                        sf::Color::White, 0)};  // split button
+
+  sf::RectangleShape hit_button{
+      RectangularButton(window, 850, 740, 100, 50, sf::Color(150, 150, 150), 2,
+                        sf::Color::White, 0)};  // hit button
+  sf::RectangleShape stand_button{
+      RectangularButton(window, 980, 740, 100, 50, sf::Color(150, 150, 150), 2,
+                        sf::Color::White, 0)};  // stand button
 
   while (window.isOpen()) {
     sf::Event event;
@@ -120,9 +161,9 @@ int main() {
       if (event.type == sf::Event::MouseButtonPressed &&
           event.mouseButton.button == sf::Mouse::Left) {
         sf::Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
-        if (button.getGlobalBounds().contains(mousePos)) {
-          your_score += 10;
-        }
+        // if (button.getGlobalBounds().contains(mousePos)) {
+        //   your_score += 10;
+        // }
       }
     }
 
@@ -132,15 +173,17 @@ int main() {
                sf::Color(210, 180, 140));
     drawcircle(window, 715, 0, 320, sf::Color::Black, 5, sf::Color::White);
 
-    drawRect(window, 660, 740, 120, 120, sf::Color(150, 150, 150), 2.f,
-             sf::Color::White, 0);  // YOU
+    drawRect(window, 660, 740, 120, 120, sf::Color(139, 0, 0), 3.f,
+             sf::Color(212, 175, 55), 0);  // YOU
+    drawRect(window, 440, 741, 200, 120, sf::Color(210, 180, 140), 3.,
+             sf::Color(101, 67, 33), 0);  // fish slot
     drawRect(window, 645, 600, 63, 88, sf::Color::White, 0.f, sf::Color::White,
              0);  // card1 you
     drawRect(window, 725, 600, 63, 88, sf::Color::White, 0.f, sf::Color::White,
              0);  // card 2 you
-    drawRect(window, 725, 100, 63, 88, sf::Color::White, 0.f, sf::Color::White,
+    drawRect(window, 725, 130, 63, 88, sf::Color::White, 0.f, sf::Color::White,
              0);  // card 2 dealer
-    drawRect(window, 645, 100, 63, 88, sf::Color::White, 0.f, sf::Color::White,
+    drawRect(window, 645, 130, 63, 88, sf::Color::White, 0.f, sf::Color::White,
              0);  // card 1 dealer
     drawRect(window, 310, 420, 63, 88, sf::Color::White, 0.f, sf::Color::White,
              45);  // carta 1 P1
@@ -156,21 +199,17 @@ int main() {
              sf::Color::Yellow, 45);  // slot carta P1
     drawRect(window, 685, 500, 63, 88, sf::Color::Transparent, 1.f,
              sf::Color::Yellow, 0);  // slot carta you
-    drawRect(window, 850, 810, 100, 50, sf::Color(150, 150, 150), 2,
-             sf::Color::White, 0);  // double button
-    drawRect(window, 980, 810, 100, 50, sf::Color(150, 150, 150), 2,
-             sf::Color::White, 0);  // split button
 
-    drawRect(window, 850, 740, 100, 50, sf::Color(150, 150, 150), 2,
-             sf::Color::White, 0);  // hit button
-    drawRect(window, 980, 740, 100, 50, sf::Color(150, 150, 150), 2,
-             sf::Color::White, 0);  // stand button
-    drawRect(window, 190, 540, 130, 130, sf::Color(150, 150, 150), 2,
-             sf::Color::White, 45);  // P1 box
-    drawRect(window, 1250, 530, 130, 130, sf::Color(150, 150, 150), 2,
-             sf::Color::White, 45);  // P2 box
+    drawRect(window, 190, 540, 130, 130, sf::Color(10, 17, 114), 3.f,
+             sf::Color(212, 175, 55), 45);  // P1 box
+    drawRect(window, 1250, 530, 130, 130, sf::Color(10, 17, 114), 3.f,
+             sf::Color(212, 175, 55), 45);  // P2 box
+    window.draw(hit_button);
+    window.draw(double_button);
+    window.draw(split_button);
+    window.draw(stand_button);
 
-    DrawText(window, font, "DEALER", 630, 20, 40, sf::Color::White, 0);
+    // DrawText(window, font, "DEALER", 630, 20, 40, sf::Color::White, 0);
     DrawText(window, font, "PLAYER 1", 160, 590, 20, sf::Color::White, 45);
     DrawText(window, font, "PLAYER 2", 1210, 640, 20, sf::Color::White, 315);
     DrawText(window, font, "STAND", 990, 750, 25, sf::Color::White, 0);
@@ -179,9 +218,11 @@ int main() {
     DrawText(window, font, "SPLIT", 867, 820, 25, sf::Color::White, 0);
     DrawText(window, font, "YOU", 675, 775, 40, sf::Color::White, 0);
     DrawText(window, font, "Current Score:", 765, 495, 15, sf::Color::Black, 0);
-
-    window.draw(button);
-    DrawText(window, font, "+10", 560, 785, 15, sf::Color::Black, 0);
+    DrawText(window, font, std::to_string(your_score), 800, 510, 50,
+             sf::Color::Black, 0);
+    DrawText(window, font, "FISH BALANCE", 470, 750, 20, sf::Color::Black, 0);
+    DrawText(window, font, std::to_string(fishes_left), 470, 805, 30,
+             sf::Color::Black, 0);
 
     for (int i = 0; i < redCircleCoords.size(); i++) {
       drawcircle(window, redCircleCoords[i].x, redCircleCoords[i].y, 10,
@@ -191,9 +232,9 @@ int main() {
     }
 
     for (const auto& letter : allLetters) window.draw(letter);
+    window.draw(sprite);
+    window.draw(sprite2);
 
-    DrawText(window, font, std::to_string(your_score), 800, 510, 50,
-             sf::Color::Black, 0);
 
     window.display();
   }
