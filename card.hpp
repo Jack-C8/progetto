e3258e166074b3d90685423a56cde4b6ec3e7dc4
Jack-C8 <giacomo.crescenzio05@gmail.com>
2025-05-07 12:05:22 +0200

    boh

--- card.hpp
+++ card.hpp
@@ -1,44 +1,32 @@
-#include <algorithm>
-#include <random>
+#ifndef EL_CARD_HPP
+#define EL_CARD_HPP
 #include <string>
 #include <vector>
+
+namespace el {
 
 struct Card {
   std::string suit;
   std::string range;
   int game_value;
-  Card(const std::string& suit, const std::string& range, int game_value)
-      : suit{suit}, range{range}, game_value{game_value} {}
+  bool face;
 
-  bool operator==(const Card& other) const {
-    if (suit == other.suit && range == other.range) {
-      return true;
-    }
-    return false;
-  }
+  Card(const std::string& suit, const std::string& range, int game_value,
+       bool face = true)
+      : suit{suit}, range{range}, game_value{game_value}, face{face} {}
 };
+
 class Deck {
-  std::vector<Card> cards;
+  std::vector<Card> deck;
 
  public:
-  Deck() {
-    std::string suits[] = {"Hearts", "Clubs", "Spades", "Diamonds"};
-    std::string ranges[] = {"2", "3",  "4", "5", "6", "7", "8",
-                            "9", "10", "J", "Q", "K", "A"};
-    int game_value[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+  Deck();
 
-    for (int i = 0; i < 4; ++i) {
-      for (int j = 0; j < 13; ++j) {
-        cards.emplace_back(suits[i], ranges[j], game_value[j]);
-      }
-    }
-  }
-  void shuffle() {
-    std::random_device rd;
-    std::default_random_engine rng(rd());
-    std::shuffle(cards.begin(), cards.end(), rng);
-  }
+  void shuffle();
 
-  size_t getNumberOfCards() const { return cards.size(); }
-  const std::vector<Card>& getCards() const { return cards; }
+  Card topCard();
+  size_t deck_size() const;
+  const std::vector<Card>& get_deck() const;
 };
+}  // namespace el
+#endif
--- card.test.cpp
+++ card.test.cpp
@@ -1,27 +1,80 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
 #include "card.hpp"
 
-TEST_CASE("Deck shuffle") {
-    element::Deck deck1;
-    element::Deck deck2;
-  
-    SUBCASE("Shuffled Deck") {
-      deck1.shuffle();
-      deck2.shuffle();
-  
-      bool different = false;
-      for (size_t i = 0; i < 52; ++i) {
-        if (!(deck1.getCards()[i] == deck2.getCards()[i])) {
-          different = true;
-          break;
-        }
-      }
-  
-      CHECK(different == true);
-    }
-    SUBCASE("Number of cards after shuffle") {
-      deck1.shuffle();
-      CHECK(deck1.getNumberOfCards() == 52);
-    }
+#include "doctest.h"
+
+TEST_CASE("Card construction") {
+  el::Card c1("Hearts", "A", 11);
+  el::Card c2("Clubs", "2", 2, false);
+  CHECK(c1.suit == "Hearts");
+  CHECK(c1.range == "A");
+  CHECK(c1.game_value == 11);
+  CHECK(c1.face == true);
+  CHECK(c2.face == false);
+}
+
+TEST_CASE("Deck basic construction") {
+  el::Deck d1;
+  el::Deck d2;
+
+  SUBCASE("Deck Basics") {
+    CHECK(d1.deck_size() == 156);
+    CHECK(d2.deck_size() == d1.deck_size());
+
+    const auto deck1 = d1.get_deck();
+    const auto deck2 = d2.get_deck();
+    CHECK(deck1[0].suit == "Hearts");
+    CHECK(deck1[0].range == "2");
+    CHECK(deck1[0].game_value == 2);
+    CHECK(deck1[0].face == true);
+    CHECK(deck1[0].suit == deck2[0].suit);
+    CHECK(deck1[0].range == deck2[0].range);
+    CHECK(deck1[0].game_value == deck2[0].game_value);
+    CHECK(deck1[0].face == deck2[0].face);
   }
+
+  // SUBCASE("Deck construction create the same deck") {
+  //   const auto deck1 = d1.get_deck();
+  //   const auto deck2 = d2.get_deck();
+
+  //   for (size_t i = 0; i < d1.deck_size(); ++i) {
+  //     CHECK(el::operator==(deck1[i], deck2[i]));
+  //   }
+  // }
+
+  SUBCASE("topCard method functions") {
+    size_t initial_size = d1.deck_size();
+    el::Card top = d1.topCard();
+    CHECK(top.suit == "Diamonds");
+    CHECK(top.range == "A");
+    CHECK(top.game_value == 11);
+    CHECK(top.face == true);
+    CHECK(d1.deck_size() == (initial_size - 1));
+    CHECK(d1.deck_size() == 155);
+  }
+  SUBCASE("topCard method multiple time") {
+    el::Card top1 = d1.topCard();
+    CHECK(d1.deck_size() == 155);
+    CHECK(top1.game_value >= 2);
+    CHECK(top1.game_value <= 11);
+
+    el::Card top2 = d1.topCard();
+    CHECK(d1.deck_size() == 154);
+    CHECK(top2.game_value >= 2);
+    CHECK(top2.game_value <= 11);
+  }
+  SUBCASE("Not enough cards, reset the game!") {
+    size_t count = (d1.deck_size() - 30);
+    for (size_t i = 0; i < count; ++i) {
+      d1.topCard();
+    }
+    CHECK_THROWS_WITH(d1.topCard(), "Not enough cards, reset the game!");
+  }
+}
+
+/*
+inline bool operator!=(Card const& left, Card const& right) {
+  return left.suit != right.suit || left.range != right.range;
+}
+aggiungo qui la ri-definizione di == perché all'interno del gioco
+è inutile ma molto utile per i vari test da fare sulle carte e sulle funzioni.*/
