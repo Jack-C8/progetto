4c9bcb8e03cc9a9fef786ccc386bfe2bf94bc192
Jonathan Figoni <jonathan.figoni@studio.unibo.it>
2025-05-21 12:54:09 +0200

    mano aggiornate

--- hand.cpp
+++ hand.cpp
@@ -5,65 +5,77 @@
 
 namespace el
 {
-    int Hand::size() {
-      return static_cast<int>(hand_.size());
-    }
+  Hand::Hand(Card c1, Card c2)
+  {
+    hand_.push_back(c1);
+    hand_.push_back(c2);
+  }
 
-    int Hand::score()
-    {
-      int score = std::accumulate(hand_.begin(), hand_.end(), 0, [](int acc, el::Card card)
-                                   { return acc + card.game_value_; });
+  int Hand::size()
+  {
+    return static_cast<int>(hand_.size());
+  }
 
-      return score;
-    }
+  const Card Hand::element(int i) const {
+    return hand_[i+1];
+  }
 
-    void Hand::Draw(el::Deck &deck)
-    {
-      el::Card top = deck.topCard();
-      hand_.emplace_back(top);
-      if (score() > 21)
-      {
-        for (auto it = hand_.begin(); it != hand_.end(); ++it)
-        {
-          if (it->range_ == "A")
-          {
-            it->game_value_ = 1;
-            break;
-          }
-        }
-      }
-    }
+  int Hand::score()
+  {
+    int score = std::accumulate(hand_.begin(), hand_.end(), 0, [](int acc, el::Card card)
+                                { return acc + card.game_value_; });
 
-    void Hand::covered_Draw(el::Deck &deck)
-    {
-      el::Card top = deck.topCard();
-      hand_.emplace_back(top);
-      top.face_ = false;
-      if (score() > 21) // se sfori dai 21 controlla se ci sono degli assi, assegna al primo
-      {                        // che trovi il valore 1 e esci dal ciclo
-        for (auto it{hand_.begin()}; it != hand_.end(); ++it)
-        {
-          if (it->range_ == "A")
-          {
-            it->game_value_ = 1;
-            break;
-          }
-        }
-      }
-    }
+    return score;
+  }
 
-    void Hand::reveal()
+  void Hand::Draw(el::Deck &deck)
+  {
+    el::Card top = deck.topCard();
+    hand_.emplace_back(top);
+    if (score() > 21)
     {
       for (auto it = hand_.begin(); it != hand_.end(); ++it)
       {
-        if (it->face_ == false)
+        if (it->range_ == "A")
         {
-          it->face_ = true;
+          it->game_value_ = 1;
+          break;
         }
       }
     }
+  }
 
-  std::vector<el::Card> Hand::hand() const {
+  void Hand::covered_Draw(el::Deck &deck)
+  {
+    el::Card top = deck.topCard();
+    hand_.emplace_back(top);
+    top.face_ = false;
+    if (score() > 21) // se sfori dai 21 controlla se ci sono degli assi, assegna al primo
+    {                 // che trovi il valore 1 e esci dal ciclo
+      for (auto it{hand_.begin()}; it != hand_.end(); ++it)
+      {
+        if (it->range_ == "A")
+        {
+          it->game_value_ = 1;
+          break;
+        }
+      }
+    }
+  }
+
+  void Hand::reveal()
+  {
+    for (auto it = hand_.begin(); it != hand_.end(); ++it)
+    {
+      if (it->face_ == false)
+      {
+        it->face_ = true;
+      }
+    }
+  }
+
+  std::vector<el::Card> Hand::hand() const
+  {
     return hand_;
   }
 
