cda6bc9ec158c843730a026c527f96e763232721
Jonathan Figoni <jonathan.figoni@studio.unibo.it>
2025-05-21 14:35:39 +0200

    hand

--- hand.cpp
+++ hand.cpp
@@ -4,94 +4,71 @@
 
 #include "card.hpp"
 
-//mettere i trattini sotto
+// mettere i trattini sotto
 
 namespace el {
+
+Hand::Hand(Card c1, Card c2) {
+  hand_.push_back(c1);
+  hand_.push_back(c2);
+}
+
 int Hand::score() {
   int score = std::accumulate(
       hand_.begin(), hand_.end(), 0,
       [](int acc, el::Card card) { return acc + card.game_value_; });
-namespace el
-{
-  Hand::Hand(Card c1, Card c2)
-  {
-    hand_.push_back(c1);
-    hand_.push_back(c2);
-  }
-
-  int Hand::size()
-  {
-    return static_cast<int>(hand_.size());
-  }
-
-<<<<<<< HEAD
-  const Card Hand::element(int i) const {
-    return hand_[i+1];
-  }
-
-  int Hand::score()
-  {
-    int score = std::accumulate(hand_.begin(), hand_.end(), 0, [](int acc, el::Card card)
-                                { return acc + card.game_value_; });
-
-    return score;
-  }
-
-  void Hand::Draw(el::Deck &deck)
-  {
-    el::Card top = deck.topCard();
-    hand_.emplace_back(top);
-    if (score() > 21)
-    {
-      for (auto it = hand_.begin(); it != hand_.end(); ++it)
-      {
-        if (it->range_ == "A")
-        {
-          it->game_value_ = 1;
-          break;
-        }
-      }
-    }
-  }
-
-  void Hand::covered_Draw(el::Deck &deck)
-  {
-    el::Card top = deck.topCard();
-    hand_.emplace_back(top);
-    top.face_ = false;
-    if (score() > 21) // se sfori dai 21 controlla se ci sono degli assi, assegna al primo
-    {                 // che trovi il valore 1 e esci dal ciclo
-      for (auto it{hand_.begin()}; it != hand_.end(); ++it)
-      {
-        if (it->range_ == "A")
-        {
-          it->game_value_ = 1;
-          break;
-        }
-      }
-    }
-  }
-
-  void Hand::reveal()
-  {
-    for (auto it = hand_.begin(); it != hand_.end(); ++it)
-    {
-      if (it->face_ == false)
-      {
-        it->face_ = true;
-      }
-    }
-  }
-
-  std::vector<el::Card> Hand::hand() const
-  {
-    return hand_;
-  }
-
-=======
-  return score;
->>>>>>> ba237a148ac22cd49a8237e0fd41cf51fd3ea7b4
 }
+
+int Hand::size() { return static_cast<int>(hand_.size()); }
+
+const Card Hand::element(int i) const { return hand_[i + 1]; }
+
+int Hand::score() {
+  int score = std::accumulate(
+      hand_.begin(), hand_.end(), 0,
+      [](int acc, el::Card card) { return acc + card.game_value_; });
+
+  return score;
+}
+
+void Hand::Draw(Deck &deck) {
+  Card top = deck.topCard();
+  hand_.emplace_back(top);
+  if (score() > 21) {
+    for (auto it = hand_.begin(); it != hand_.end(); ++it) {
+      if (it->range_ == "A") {
+        it->game_value_ = 1;
+        break;
+      }
+    }
+  }
+}
+
+void Hand::covered_Draw(Deck &deck) {
+  Card top = deck.topCard();
+  hand_.emplace_back(top);
+  top.face_ = false;
+  if (score() > 21)  // se sfori dai 21 controlla se ci sono degli assi,
+                     // assegna al primo
+  {                  // che trovi il valore 1 e esci dal ciclo
+    for (auto it{hand_.begin()}; it != hand_.end(); ++it) {
+      if (it->range_ == "A") {
+        it->game_value_ = 1;
+        break;
+      }
+    }
+  }
+}
+
+void Hand::reveal() {
+  for (auto it = hand_.begin(); it != hand_.end(); ++it) {
+    if (it->face_ == false) {
+      it->face_ = true;
+    }
+  }
+}
+
+std::vector<el::Card> Hand::hand() const { return hand_; }
 
 void Hand::Draw(el::Deck &deck) {
   el::Card top = deck.topCard();
@@ -105,7 +82,6 @@ void Hand::Draw(el::Deck &deck) {
     }
   }
 }
-
 void Hand::covered_Draw(el::Deck &deck) {
   el::Card top = deck.topCard();
   hand_.emplace_back(top);
@@ -131,15 +107,4 @@ void Hand::reveal() {
 }
 
 std::vector<el::Card> Hand::hand() const { return hand_; }
-
-}  // namespace hd
-/*la funzione di calcolo è implementata correttamente (spero)
-per quanto riguarda il calcolo del valore dell'asso credo di aggiungere una
-funzione check all'interno da fare girare di default solo per controllare se la
-mano ha un asso
-
-in caso positivo bisogna creare una funzione che viene chiamata e chiede al
-giocatore quale valore attribuire a tutti gli assi che ha, banalmente direi con
-un ciclo for che scorre tutte le carte e di quelle contrassegnate da range == A
-lo chiede, una persona può pescare anche tutti e 12 gli assi, sarebbe un figlio
-di puttana? sì.*/
+}  
