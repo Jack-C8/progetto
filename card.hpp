b394cc3985759101e6a03d87538634d233181f8a
Jack-C8 <giacomo.crescenzio05@gmail.com>
2025-05-21 12:38:49 +0200

    Card funzionanti

--- card.hpp
+++ card.hpp
@@ -1,38 +1,38 @@
 #ifndef EL_CARD_HPP
 #define EL_CARD_HPP
-
+#include <algorithm>
+#include <random>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
-namespace el
-{
+namespace el {
 
-  struct Card
-  {
-    std::string suit_;
-    std::string range_;
-    int game_value_;
-    bool face_;
+struct Card {
+  std::string suit_;
+  std::string range_;
+  int game_value_;
+  bool face_;
 
-    Card(const std::string &suit, const std::string &range, int const game_value);
-  };
+  Card(const std::string &suit_, const std::string &range_,
+       int const game_value_, bool face_);
+};
 
-  bool operator==(const Card& one, const Card &other);
+bool operator==(const Card &one, const Card &other);
 
-  class Deck
-  {
-    std::vector<Card> deck_;
+class Deck {
+  std::vector<Card> deck_;
 
-  public:
-    Deck();
+ public:
+  Deck();
 
-    void shuffle();
+  void shuffle();
 
-    Card topCard();
+  Card topCard();
 
-    size_t deck_size() const;
+  size_t deck_size() const;
 
-    const std::vector<Card>& get_deck() const ;
-  };
-} // namespace el
+  const std::vector<Card> &get_deck() const;
+};
+}  // namespace el
 #endif
--- card.test.cpp
+++ card.test.cpp
@@ -3,18 +3,6 @@
 #include "card.hpp"
 #include "doctest.h"
 
-TEST_CASE("Card construction") {
-  el::Card c1("Hearts", "A", 11);
-  el::Card c2("Clubs", "2", 2);
-  c2.face_ = false;
-
-  CHECK(c1.suit_ == "Hearts");
-  CHECK(c1.range_ == "A");
-  CHECK(c1.game_value_ == 11);
-  CHECK(c1.face_ == true);
-  CHECK(c2.face_ == false);
-}
-
 TEST_CASE("Deck basic construction") {
   el::Deck d1;
   el::Deck d2;
@@ -36,19 +24,18 @@ TEST_CASE("Deck basic construction") {
     CHECK(deck1[0].face_ == deck2[0].face_);
   }
 
-  // SUBCASE("Deck construction create the same deck") {
-  //   const auto deck1 = d1.get_deck();
-  //   const auto deck2 = d2.get_deck();
+  SUBCASE("Deck construction create the same deck") {
+    const auto deck1 = d1.get_deck();
+    const auto deck2 = d2.get_deck();
 
-  //   for (size_t i = 0; i < d1.deck_size(); ++i) {
-  //     CHECK(el::operator==(deck1[i], deck2[i]));
-  //   }
-  // }
+    for (size_t i = 0; i < d1.deck_size(); ++i) {
+      CHECK(el::operator==(deck1[i], deck2[i]));
+    }
+  }
 
   SUBCASE("topCard method functions") {
     size_t initial_size = d1.deck_size();
     el::Card top = d1.topCard();
-
     CHECK(top.suit_ == "Diamonds");
     CHECK(top.range_ == "A");
     CHECK(top.game_value_ == 11);
@@ -56,6 +43,7 @@ TEST_CASE("Deck basic construction") {
     CHECK(d1.deck_size() == (initial_size - 1));
     CHECK(d1.deck_size() == 155);
   }
+
   SUBCASE("topCard method multiple time") {
     el::Card top1 = d1.topCard();
 
@@ -75,11 +63,22 @@ TEST_CASE("Deck basic construction") {
     }
     CHECK_THROWS_WITH(d1.topCard(), "Not enough cards, reset the game!");
   }
+  SUBCASE("Card operator==") {
+    el::Card c1{"Hearts", "2", 2, true};
+    el::Card c2{"Hearts", "3", 3, true};
+    el::Card c3{"Clubs", "2", 2, true};
+    el::Card c4{"Hearts", "2", 3, true};
+    el::Card c5{"Spades", "4", 4, true};
+    CHECK((c1 == c1) == true);
+    CHECK((c1 == c2) == false);
+    CHECK((c1 == c3) == false);
+    CHECK((c1 == c4) ==
+          true);  // operator== non deve tener conto del game_value della carta
+    CHECK((c1 == c5) == false);
+  }
+  SUBCASE("Draw the last constructed card") {
+    el::Card card{"Diamonds", "A", 11, true};
+    CHECK(d1.topCard() == card);
+    CHECK(d1.deck_size() == 155);
+  }
 }
-
-/*
-inline bool operator!=(Card const& left, Card const& right) {
-  return left.suit__ != right.suit_ || left.range_ != right.range_;
-}
-aggiungo qui la ri-definizione di == perché all'interno del gioco
-è inutile ma molto utile per i vari test da fare sulle carte e sulle funzioni.*/
